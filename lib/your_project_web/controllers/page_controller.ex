e0f1074b9fedd1a955239774ffffec724a9e5d97
Nicholas Moen <arcanemachine@tutanota.com>
2024-04-07 14:09:51 -0600

    Demo: Create example API route so we can see what happens when calling our new exception

--- lib/your_project_web/controllers/page_controller.ex
+++ lib/your_project_web/controllers/page_controller.ex
@@ -1,9 +1,9 @@
 defmodule YourProjectWeb.PageController do
   use YourProjectWeb, :controller
 
-  def home(conn, _params) do
+  def home(_conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
-    render(conn, :home, layout: false)
+    raise YourProject.PlugException, plug_status: 400, message: "Hello world!"
   end
 end
