f24d9a526f1ececcfba2132518fb552e5d58ab96
Nicholas Moen <arcanemachine@tutanota.com>
2024-04-07 05:16:36 -0600

    Add custom clause to `YourProjectWeb.ErrorView` to display custom error message

--- lib/your_project_web/views/error_view.ex
+++ lib/your_project_web/views/error_view.ex
@@ -7,6 +7,15 @@ defmodule YourProjectWeb.ErrorView do
   #   "Internal Server Error"
   # end
 
+  @doc "Render a JSON response with custom message."
+  def render(
+        <<_status::binary-3>> <> ".json",
+        %{conn: %{assigns: %{reason: %YourProject.PlugException{message: message}}}}
+      )
+      when message != "" do
+    message
+  end
+
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.html" becomes
   # "Not Found".
